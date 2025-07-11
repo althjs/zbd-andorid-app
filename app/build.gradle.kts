e439ddd5c1294832d734d7c8c34b4d64870783d5
Jongsoon Park <althjs@gmail.com>
2025-07-11 20:41:20 +0900

    chore: Android 15(API 35) 대응 및 빌드 환경 최신화
    - compileSdk, targetSdk 35로 상향
    - gradle.properties에 suppressUnsupportedCompileSdk 옵션 추가
    - libs.versions.toml 동기화
    - .aab 파일 최신화

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "kr.zbd.android"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "kr.zbd.android"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 6
         versionName = "1.0.5"
 
